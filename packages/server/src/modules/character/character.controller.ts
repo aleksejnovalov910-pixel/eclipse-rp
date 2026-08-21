12a3809a74dfeb9a08a589e0518cbfcc76012c61
aleksejnovalov910-pixel <aleksejnovalov910@gmail.com>
2026-08-21 23:30:33 +0300

    feat(combat): restore weapon loadout on character spawn

--- packages/server/src/modules/character/character.controller.ts
+++ packages/server/src/modules/character/character.controller.ts
@@ -16,6 +16,7 @@ import { advanceQuestSafe } from '../quests/quest.service';
 import { currentOutfit,currentTattoos } from '../customization/customization.service';
 import { custodyState } from '../publicServices/policeActions.service';
 import { hospitalize, state as medicalState } from '../publicServices/medicalEmergency.service';
+import { syncLoadout } from '../combat/weaponShop.service';
 
 const log = createLogger('character:rpc');
 
@@ -59,18 +60,16 @@ export const registerCharacterModule = (): void => {
     const [outfit,tattoos,custody,medicalRaw]=await Promise.all([currentOutfit(result.data.characterId),currentTattoos(result.data.characterId),custodyState(result.data.characterId),medicalState(result.data.characterId)]);
     ctx.player.call(ServerEvent.OutfitState, [JSON.stringify(outfit.components)]);
     ctx.player.call(ServerEvent.TattooState, [JSON.stringify(tattoos)]);
-    if(custody.jailedUntil&&new Date(custody.jailedUntil).getTime()>Date.now()){
-      ctx.player.position=new mp.Vector3(1690.8,2591.3,45.9);
-      ctx.player.dimension=0;
-    }
+    const jailed=custody.jailedUntil!==null&&new Date(custody.jailedUntil).getTime()>Date.now();
+    if(jailed){ctx.player.position=new mp.Vector3(1690.8,2591.3,45.9);ctx.player.dimension=0;}
     ctx.player.call(ServerEvent.PoliceCustodyState,[JSON.stringify(custody)]);
     let medical=medicalRaw;
     const bleedoutExpired=medical.downed&&medical.bleedoutAt!==null&&new Date(medical.bleedoutAt).getTime()<=Date.now();
     if(bleedoutExpired)medical=await hospitalize(result.data.characterId);
-    if(medical.downed){ctx.player.health=20;}else if(medical.hospitalizedUntil&&new Date(medical.hospitalizedUntil).getTime()>Date.now()){
-      ctx.player.position=new mp.Vector3(307.3,-595.3,43.3);ctx.player.dimension=0;ctx.player.health=100;ctx.player.armour=0;
-    }
+    const hospitalized=medical.hospitalizedUntil!==null&&new Date(medical.hospitalizedUntil).getTime()>Date.now();
+    if(medical.downed){ctx.player.health=20;}else if(hospitalized){ctx.player.position=new mp.Vector3(307.3,-595.3,43.3);ctx.player.dimension=0;ctx.player.health=100;ctx.player.armour=0;}
     ctx.player.call(ServerEvent.MedicalState,[JSON.stringify(medical)]);
+    if(jailed||medical.downed||hospitalized)ctx.player.call(ServerEvent.WeaponLoadout,[JSON.stringify([])]);else await syncLoadout(result.data.characterId,ctx.player);
     ctx.player.call(ServerEvent.SessionState, [SessionState.Playing]);
     await advanceQuestSafe(result.data.characterId, 'welcome');
     return { ok: true, data: { characterId: result.data.characterId } };
