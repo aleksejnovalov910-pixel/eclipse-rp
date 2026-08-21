4e32ad126dec2b85c56625a0b2e95e12710b552f
aleksejnovalov910-pixel <aleksejnovalov910@gmail.com>
2026-08-21 17:06:35 +0300

    feat(world): expose documents and criminal rpc to CEF

---
+++ packages/client/src/modules/worldSocial.ts
@@ -0,0 +1,6 @@
+import { RpcEvent } from '@eclipse/shared';
+import { allowFromCef } from '../core/cefBridge';
+export const registerWorldSocialModule=():void=>allowFromCef(
+ RpcEvent.DocumentGet,RpcEvent.DocumentIssueLicense,RpcEvent.DocumentRevokeLicense,
+ RpcEvent.CriminalGet,RpcEvent.CriminalTerritories,RpcEvent.CriminalContracts,RpcEvent.CriminalSetRank,RpcEvent.CriminalContractProgress
+);
