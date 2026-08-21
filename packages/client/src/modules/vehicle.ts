75d3e7df42bd0d4a451e176304f56cdcf82b2ff4
aleksejnovalov910-pixel <aleksejnovalov910@gmail.com>
2026-08-21 22:32:16 +0300

    feat(tuning): bridge visual tuning preview

--- packages/client/src/modules/vehicle.ts
+++ packages/client/src/modules/vehicle.ts
@@ -1,23 +1,4 @@
 import { RpcEvent } from '@eclipse/shared';
 import { allowFromCef } from '../core/cefBridge';
-
-export const registerVehicleModule = (): void => {
-  allowFromCef(
-    RpcEvent.VehicleList,
-    RpcEvent.VehicleUsableList,
-    RpcEvent.VehicleSpawn,
-    RpcEvent.VehicleStore,
-    RpcEvent.VehicleToggleLock,
-    RpcEvent.VehicleAccessList,
-    RpcEvent.VehicleAccessGrant,
-    RpcEvent.VehicleAccessRevoke,
-    RpcEvent.VehicleServiceShops,
-    RpcEvent.VehicleRepairQuote,
-    RpcEvent.VehicleRepair,
-    RpcEvent.VehicleTuningOptions,
-    RpcEvent.VehicleTuningState,
-    RpcEvent.VehicleTuningInstall,
-    RpcEvent.FuelStations,
-    RpcEvent.VehicleRefuel,
-  );
-};
+const PREVIEW='eclipse:vehicle:tuningPreview';
+export const registerVehicleModule=():void=>{allowFromCef(RpcEvent.VehicleList,RpcEvent.VehicleUsableList,RpcEvent.VehicleSpawn,RpcEvent.VehicleStore,RpcEvent.VehicleToggleLock,RpcEvent.VehicleAccessList,RpcEvent.VehicleAccessGrant,RpcEvent.VehicleAccessRevoke,RpcEvent.VehicleServiceShops,RpcEvent.VehicleRepairQuote,RpcEvent.VehicleRepair,RpcEvent.VehicleTuningOptions,RpcEvent.VehicleTuningState,RpcEvent.VehicleTuningInstall,PREVIEW,RpcEvent.FuelStations,RpcEvent.VehicleRefuel);};
