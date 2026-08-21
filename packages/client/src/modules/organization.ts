3c1cd38a45f047fcc555ca39a744eaa7201843eb
aleksejnovalov910-pixel <aleksejnovalov910@gmail.com>
2026-08-21 16:20:36 +0300

    feat(public-services): expose MDT and EMS rpc to CEF

--- packages/client/src/modules/organization.ts
+++ packages/client/src/modules/organization.ts
@@ -2,5 +2,6 @@ import { RpcEvent } from '@eclipse/shared';
 import { allowFromCef } from '../core/cefBridge';
 export const registerOrganizationModule=():void=>allowFromCef(
  RpcEvent.OrganizationGet,RpcEvent.OrganizationMembers,RpcEvent.OrganizationDuty,RpcEvent.OrganizationCalls,
- RpcEvent.OrganizationCreateCall,RpcEvent.OrganizationAssignCall,RpcEvent.OrganizationCloseCall
+ RpcEvent.OrganizationCreateCall,RpcEvent.OrganizationAssignCall,RpcEvent.OrganizationCloseCall,
+ RpcEvent.PoliceCitizen,RpcEvent.PoliceAddRecord,RpcEvent.PoliceResolveRecord,RpcEvent.EmsPatient,RpcEvent.EmsTreat
 );
