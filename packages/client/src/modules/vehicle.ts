871a913db6c2f510e5cfec27bfd204179d6ad6dc
aleksejnovalov910-pixel <aleksejnovalov910@gmail.com>
2026-08-21 03:16:49 +0300

    feat(vehicle): allow service and access RPCs from CEF

--- packages/client/src/modules/vehicle.ts
+++ packages/client/src/modules/vehicle.ts
@@ -4,9 +4,16 @@ import { allowFromCef } from '../core/cefBridge';
 export const registerVehicleModule = (): void => {
   allowFromCef(
     RpcEvent.VehicleList,
+    RpcEvent.VehicleUsableList,
     RpcEvent.VehicleSpawn,
     RpcEvent.VehicleStore,
     RpcEvent.VehicleToggleLock,
+    RpcEvent.VehicleAccessList,
+    RpcEvent.VehicleAccessGrant,
+    RpcEvent.VehicleAccessRevoke,
+    RpcEvent.VehicleServiceShops,
+    RpcEvent.VehicleRepairQuote,
+    RpcEvent.VehicleRepair,
     RpcEvent.FuelStations,
     RpcEvent.VehicleRefuel,
   );
